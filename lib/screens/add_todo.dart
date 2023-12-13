784022038b8581d91f2babd06fe9fab471f06e75
Grand Marcell <grand1310marcell@gmail.com>
2023-12-13 11:32:19 +0700

    add todo

--- lib/screens/add_todo.dart
+++ lib/screens/add_todo.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +11,9 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleConroller = TextEditingController();
+  TextEditingController descriptionConroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +24,14 @@ class _AddTodoPageState extends State<AddTodoPage> {
         padding: EdgeInsets.all(20),
         children: [
           TextField(
+            controller: titleConroller,
             decoration: InputDecoration(hintText: 'Title'),
           ),
           SizedBox(
             height: 20,
           ),
           TextField(
+            controller: descriptionConroller,
             decoration: InputDecoration(hintText: 'Description'),
             keyboardType: TextInputType.multiline,
             minLines: 5,
@@ -32,9 +40,46 @@ class _AddTodoPageState extends State<AddTodoPage> {
           SizedBox(
             height: 20,
           ),
-          ElevatedButton(onPressed: () {}, child: Text('Submit'))
+          ElevatedButton(onPressed: submitData, child: Text('Submit'))
         ],
       ),
     );
+  }
+
+  Future<void> submitData() async {
+    // Get Data From Input
+    final title = titleConroller.text;
+    final desc = descriptionConroller.text;
+    final body = {"title": title, "description": desc, "is_completed": false};
+    // Submit To API
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+    // Show Status Success or Fail
+
+    if (response.statusCode == 201) {
+      titleConroller.text = '';
+      descriptionConroller.text = '';
+      showSucessMessage('Creation Success');
+    } else {
+      showErrorMessage('Creation Failed');
+    }
+  }
+
+  void showSucessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
